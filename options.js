ee752bae90334d1a4f1ae8297da502943f8b30ef
Ng Zhi An <ngzhian@gmail.com>
2016-12-03 15:00:02 +0800

    Add options for hover delay

--- options.js
+++ options.js
@@ -1,10 +1,18 @@
+const DEFAULT_PLAYBACK_RATE = '1';
+const DEFAULT_HOVER_TIME = '370';
+
+const playbackRateControl = document.getElementById('playback-rate');
+const hoverTimeControl = document.getElementById('hover-time');
+const resetControl = document.getElementById('reset-to-default');
+const status = document.getElementById('status');
+
+
 function save_options() {
-  var playbackRate = document.getElementById('playback-rate').value;
   chrome.storage.sync.set({
-    playbackRate: playbackRate,
+    playbackRate: playbackRateControl.value,
+    hoverTime: hoverTimeControl.value,
   }, function() {
     // Update status to let user know options were saved.
-    var status = document.getElementById('status');
     status.textContent = 'Options saved.';
     setTimeout(function() {
       status.textContent = '';
@@ -14,12 +22,21 @@ function save_options() {
 
 function restore_options() {
   chrome.storage.sync.get({
-    playbackRate: '1',
+    playbackRate: DEFAULT_PLAYBACK_RATE,
+    hoverTime: DEFAULT_HOVER_TIME,
   }, function(items) {
-    document.getElementById('playback-rate').value = items.playbackRate;
+    playbackRateControl.value = items.playbackRate;
+    hoverTimeControl.value = items.hoverTime;
   });
+}
+
+function reset_and_save() {
+  playbackRateControl.value = DEFAULT_PLAYBACK_RATE;
+  hoverTimeControl.value = DEFAULT_HOVER_TIME;
+  save_options();
 }
 
 document.addEventListener('DOMContentLoaded', restore_options);
 document.getElementById('save').addEventListener('click',
     save_options);
+document.getElementById('reset-to-default').addEventListener('click', reset_and_save);
